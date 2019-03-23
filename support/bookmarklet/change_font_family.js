6daecee7d2f8927c0b58bb8083ed90b278cebfe9
nomrand <42882840+nomrand@users.noreply.github.com>
2019-03-23 10:55:18 +0900

    add bookmarklet of changeFontFamily(GoogleFonts)

---
+++ support/bookmarklet/change_font_family.js
@@ -0,0 +1,22 @@
+/**
+ * Change Page Font-Family to specific google-fonts family name.
+ * ex.
+ *  changeFontFamilyGF('Kanit');
+ *  changeFontFamilyGF('Pridi');
+ * @param {*} ff font-family(for google-fonts)
+ */
+function changeFontFamilyGF(ff) {
+    var head = document.getElementsByTagName('head');
+
+    var link = document.createElement('link');
+    link.setAttribute('href', 'https:\/\/fonts.googleapis.com\/css?family=' + ff);
+    link.setAttribute('rel', 'stylesheet');
+
+    var style = document.createElement('style');
+    style.setAttribute('type', 'text\/css');
+    style.innerHTML = '* {font-family: ' + ff + ' !important;}';
+
+    head[0].appendChild(link);
+    head[0].appendChild(style);
+}
+
