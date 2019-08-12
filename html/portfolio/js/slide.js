567d8a92780fe9bd00c8ecbea6cd332b9dba09f1
nomrand <42882840+nomrand@users.noreply.github.com>
2019-08-12 12:20:25 +0700

    index finish

--- html/portfolio/js/slide.js
+++ html/portfolio/js/slide.js
@@ -6,33 +6,61 @@ const SLIDE_IMAGE = [
     "img/test4.png",
 ];
 
-$(function(){
-    slide();
+$(function () {
+    // initSlide();
+    // slide();
 });
+
+function initSlide() {
+    $('#subcont_table').css({
+        top: $('#subcont_tr').height() + "px",
+    });
+}
 
 let cur_slide = 0;
 function slide() {
     cur_slide++;
+    if (cur_slide >= SLIDE_IMAGE.length) {
+        initSlide();
+    }
 
     // IMAGES
+    // Before Image fade out
     $('#subcont_tr td:first img').animate({
         opacity: 0,
-    }, { duration: SLIDE_DURATION, easing: 'linear',
-        complete: function(){
-            this.remove();
-        }
-    });
+    }, {
+            duration: SLIDE_DURATION, easing: 'linear',
+            complete: function () {
+                this.remove();
+            }
+        });
+    // After Image fade in
     let img = $("<img>");
     img.attr("src", SLIDE_IMAGE[cur_slide]);
-    img.css({"opacity": 0});
+    img.css({ "opacity": 0 });
     $('#subcont_tr td:first').append(img);
     img.animate({
         opacity: 1,
-    }, { duration: SLIDE_DURATION, easing: 'linear', });
+    }, {
+            duration: SLIDE_DURATION, easing: 'linear',
+            complete: function () {
+                cur_slide++;
+                if (cur_slide >= SLIDE_IMAGE.length) {
+                    cur_slide = 0;
+                }
+            }
+        });
 
     // TEXTS
     $('#subcont_table').animate({
-        top: (cur_slide * $("#subcont_table td").height()) + 'px'
-    }, { duration: SLIDE_DURATION, easing: 'linear', });
+        top: $("#subcont_table td").height(),
+    }, {
+            duration: SLIDE_DURATION, easing: 'linear',
+            complete: function () {
+                if (cur_slide >= SLIDE_IMAGE.length) {
+                    cur_slide = 0;
+                }
+            }
+        });
     setTimeout('slide()', SLIDE_DURATION);
 }
