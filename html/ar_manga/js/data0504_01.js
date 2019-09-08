40eb91f071a4da12438fe2812d9de8b36c79aa97
nomrand <42882840+nomrand@users.noreply.github.com>
2019-09-08 22:11:10 +0700

    manga work received 1st

---
+++ html/ar_manga/js/data0504_01.js
@@ -0,0 +1,14 @@
+data["0504_01"] = [
+    { img: "./0504/ar5410/ar/pattern-D3ERIKsUYAASHLi2.png", patt: "./0504/ar5410/ar/pattern-D3ERIKsUYAASHLi2.patt", },
+    { img: "./0504/ar5410/ar/pattern-D3ERIKsUYAASHLi3.png", patt: "./0504/ar5410/ar/pattern-D3ERIKsUYAASHLi3.patt", },
+    { img: "./0504/ar5410/ar/pattern-D3ERIKsUYAASHLi5.png", patt: "./0504/ar5410/ar/pattern-D3ERIKsUYAASHLi5.patt", },
+    { img: "./0504/ar5410/ar/pattern-D3ERIKsUYAASHLi6.png", patt: "./0504/ar5410/ar/pattern-D3ERIKsUYAASHLi6.patt", },
+    { img: "./0504/ar5411/ar/pattern-m012.png", patt: "./0504/ar5411/ar/pattern-m012.patt", },
+    { img: "./0504/ar5411/ar/pattern-m123.png", patt: "./0504/ar5411/ar/pattern-m123.patt", },
+    { img: "./0504/ar5411/ar/pattern-m456.png", patt: "./0504/ar5411/ar/pattern-m456.patt", },
+    { img: "./0504/ar5411/ar/pattern-m789.png", patt: "./0504/ar5411/ar/pattern-m789.patt", },
+    { img: "./0504/ar5412/ar/001.png", patt: "./0504/ar5412/ar/001.patt", },
+    { img: "./0504/ar5412/ar/002.png", patt: "./0504/ar5412/ar/002.patt", },
+    { img: "./0504/ar5412/ar/003.png", patt: "./0504/ar5412/ar/003.patt", },
+    { img: "./0504/ar5412/ar/004.png", patt: "./0504/ar5412/ar/004.patt", },
+];
---
+++ html/ar_manga/js/data0505_01.js
@@ -0,0 +1,18 @@
+data["0505_01"] = [
+    { img: "./0505/ar5503/ar/AR11.png", patt: "./0505/ar5503/ar/AR11.patt", },
+    { img: "./0505/ar5503/ar/AR22.png", patt: "./0505/ar5503/ar/AR22.patt", },
+    { img: "./0505/ar5503/ar/AR33.png", patt: "./0505/ar5503/ar/AR33.patt", },
+    { img: "./0505/ar5503/ar/AR44.png", patt: "./0505/ar5503/ar/AR44.patt", },
+    { img: "./0505/ar5510/ar/001.png", patt: "./0505/ar5510/ar/001.patt", },
+    { img: "./0505/ar5510/ar/002.png", patt: "./0505/ar5510/ar/002.patt", },
+    { img: "./0505/ar5510/ar/003.png", patt: "./0505/ar5510/ar/003.patt", },
+    { img: "./0505/ar5510/ar/004.png", patt: "./0505/ar5510/ar/004.patt", },
+    { img: "./0505/ar5511/AR/1.png", patt: "./0505/ar5511/AR/1.patt", },
+    { img: "./0505/ar5511/AR/2.png", patt: "./0505/ar5511/AR/2.patt", },
+    { img: "./0505/ar5511/AR/3.png", patt: "./0505/ar5511/AR/3.patt", },
+    { img: "./0505/ar5511/AR/4.png", patt: "./0505/ar5511/AR/4.patt", },
+    { img: "./0505/ar5512/ar/001.png", patt: "./0505/ar5512/ar/001.patt", },
+    { img: "./0505/ar5512/ar/002.png", patt: "./0505/ar5512/ar/002.patt", },
+    { img: "./0505/ar5512/ar/003.png", patt: "./0505/ar5512/ar/003.patt", },
+    { img: "./0505/ar5512/ar/004.png", patt: "./0505/ar5512/ar/004.patt", },
+];
